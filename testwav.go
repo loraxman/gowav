cf69bfcb73e9809ab2637bb1c4517698d8f125e0
Roger Kelly <roger.kelly@sabre.com>
2013-07-19 21:48:05 -0500

    format

--- testwav.go
+++ testwav.go
@@ -1,29 +1,29 @@
 package main
 
 import (
-	"wave"
-	"fmt"
 	"flag"
+	"fmt"
+	"wave"
 )
 
 func main() {
-		var fileName string
-		flag.StringVar(&fileName, "fileName", "test.wav", "pass the filename for the wav")
-		flag.Parse()
-		wv := wave.OpenWav(fileName,"r")
-		fmt.Printf("%s\t%d\t%s\t%s\t%d\t%d\t%d\t%d\t%d\n",  wv.Riff, wv.ChunkSize, wv.Wave, wv.Fmt, wv.AudioFormat, wv.NumOfChan, 
+	var fileName string
+	flag.StringVar(&fileName, "fileName", "test.wav", "pass the filename for the wav")
+	flag.Parse()
+	wv := wave.OpenWav(fileName, "r")
+	fmt.Printf("%s\t%d\t%s\t%s\t%d\t%d\t%d\t%d\t%d\n", wv.Riff, wv.ChunkSize, wv.Wave, wv.Fmt, wv.AudioFormat, wv.NumOfChan,
 		wv.SampleRate, wv.DataSize, wv.BitsPerSample)
 
-		fmt.Printf("%d\t%d\n", wv.GetNChannels(), wv.GetNFrames())
-		data := wv.ReadFrames(30)
-		fmt.Printf("%s\n", data)
-		a,b,c,d := wv.GetParams()
-		fmt.Printf("%d\t%d\t%d\t%d\n", a,b,c,d)
+	fmt.Printf("%d\t%d\n", wv.GetNChannels(), wv.GetNFrames())
+	data := wv.ReadFrames(30)
+	fmt.Printf("%s\n", data)
+	a, b, c, d := wv.GetParams()
+	fmt.Printf("%d\t%d\t%d\t%d\n", a, b, c, d)
 
-		wv2 := wave.OpenWav("test2.wav","w")
-		wv2.SetNChannels(2)
-		wv2.SetFrameRate(48000)
-		wv2.SetSampWidth(2)
-		wv2.WriteFrames(&data)
-		wv2.Close()
+	wv2 := wave.OpenWav("test2.wav", "w")
+	wv2.SetNChannels(2)
+	wv2.SetFrameRate(48000)
+	wv2.SetSampWidth(2)
+	wv2.WriteFrames(&data)
+	wv2.Close()
 }
--- wave.go
+++ wave.go
@@ -1,56 +1,53 @@
 package wave
 
 import (
-	"os"
-	"io"
 	"bytes"
 	"encoding/binary"
+	"io"
+	"os"
 )
 
 type WaveFile struct {
-	Riff	string	
-	ChunkSize uint32
-	Wave	string
-	Fmt	string
+	Riff          string
+	ChunkSize     uint32
+	Wave          string
+	Fmt           string
 	Subchunk1Size uint32
-	AudioFormat uint16
-	NumOfChan	uint16
-	SampleRate	uint32
-	BytesPerSec uint16
-	BlockAlign uint16
+	AudioFormat   uint16
+	NumOfChan     uint16
+	SampleRate    uint32
+	BytesPerSec   uint16
+	BlockAlign    uint16
 	BitsPerSample uint16
-	Subchunk2ID string
-	DataSize uint32
-	Data []byte
-	
+	Subchunk2ID   string
+	DataSize      uint32
+	Data          []byte
+
 	ReadPtr int
 
-	FilePtr  *os.File
-
+	FilePtr *os.File
 }
 
-func OpenWav(filename string, mode string) (*WaveFile) {
-	
+func OpenWav(filename string, mode string) *WaveFile {
+
 	var f *os.File
 	var err error
-	
-	if (mode == "w") {
-		f,err = os.Create(filename)
+
+	if mode == "w" {
+		f, err = os.Create(filename)
 	}
-	if (mode == "r") {
-		f,err = os.Open(filename)
+	if mode == "r" {
+		f, err = os.Open(filename)
 	}
 
-	
 	if err != nil {
-   		panic(err)
-   	}
+		panic(err)
+	}
 
-	wv := readWav(f) 
+	wv := readWav(f)
 	wv.FilePtr = f
 	return wv
 }
-
 
 func (wv WaveFile) GetNChannels() int {
 	return int(wv.NumOfChan)
@@ -64,14 +61,13 @@ func (wv WaveFile) SetSampWidth(n int) {
 	wv.BitsPerSample = uint16(n) * 8
 }
 
-
 func (wv WaveFile) SetFrameRate(n int) {
-	wv.SampleRate = uint32(n)	
+	wv.SampleRate = uint32(n)
 }
 
 func (wv WaveFile) GetSampWidth() int {
 
-	return int(wv.BitsPerSample/8)
+	return int(wv.BitsPerSample / 8)
 
 }
 
@@ -81,42 +77,38 @@ func (wv WaveFile) GetFrameRate() int {
 
 }
 
-
-
-func (wv WaveFile) WriteFrames(data *[] byte) {
-//Write audio frames and make sure nframes is correct.
-	if len(*data)/(wv.GetNChannels() * wv.GetSampWidth()) != wv.GetNFrames() {
-		panic ("frame size not valid")
+func (wv WaveFile) WriteFrames(data *[]byte) {
+	//Write audio frames and make sure nframes is correct.
+	if len(*data)/(wv.GetNChannels()*wv.GetSampWidth()) != wv.GetNFrames() {
+		panic("frame size not valid")
 	}
-	
-	wv.FilePtr.Write(*data) 
-	
+
+	wv.FilePtr.Write(*data)
+
 }
 
+func (wv WaveFile) WriteFramesRaw(data *[]byte) {
+	//Write audio frames with no check of nframes
 
-func (wv WaveFile) WriteFramesRaw(data *[] byte) {
-//Write audio frames with no check of nframes
-	
-	wv.FilePtr.Write(*data) 
-	
+	wv.FilePtr.Write(*data)
+
 }
-
 
 func (wv WaveFile) GetNFrames() int {
 
-	numSamples  := wv.DataSize/wv.SampleRate
-	numChan := uint32( wv.NumOfChan)
-	return int(numSamples/numChan)
+	numSamples := wv.DataSize / wv.SampleRate
+	numChan := uint32(wv.NumOfChan)
+	return int(numSamples / numChan)
 
 }
 
 func (wv WaveFile) ReadFrames(n int) []byte {
-  // n frame reads is reading length of n * sampwidth * channels 
+	// n frame reads is reading length of n * sampwidth * channels
 	bytesToRead := n * wv.GetSampWidth() * wv.GetNChannels()
 	defer func() {
 		wv.ReadPtr += bytesToRead
 	}()
-	return wv.Data[wv.ReadPtr:bytesToRead+wv.ReadPtr]
+	return wv.Data[wv.ReadPtr : bytesToRead+wv.ReadPtr]
 
 }
 
@@ -124,7 +116,6 @@ func (wv WaveFile) Close() {
 	wv.FilePtr.Close()
 
 }
-
 
 func (wv WaveFile) Rewind() {
 	wv.ReadPtr = 0
@@ -138,67 +129,65 @@ func (wv WaveFile) Tell() int {
 	return wv.ReadPtr
 }
 
-
-func (wv WaveFile) GetParams() (int,int,int,int) {
-	return wv.GetNChannels(),wv.GetSampWidth(),wv.GetFrameRate(),wv.GetNFrames()
-}
- 
-func readWav(f *os.File) (*WaveFile) {
-	  //make slice for the 44 byte header
-		data := make([]byte, 44)
-		_, err:= f.Read(data)
-		if err != nil {
-		           panic("file error")
-		}
-		wv := new(WaveFile)
-		wv.Riff = string(data[0:4])
-
-		// conversion needed to move the raw binary data to integer value
-		// create a buffer of bytes to hold the data
-		buf := bytes.NewBuffer(data[4:8])
-		// read the data in the buffer into the integer using LittleEndian conversion
-		binary.Read(buf, binary.LittleEndian, &wv.ChunkSize)
-		wv.Wave = string(data[8:12])
-		wv.Fmt = string(data[12:16])
-		buf = bytes.NewBuffer(data[16:20])
-		binary.Read(buf, binary.LittleEndian, &wv.Subchunk1Size)
-
-		buf = bytes.NewBuffer(data[20:22])
-		binary.Read(buf, binary.LittleEndian, &wv.AudioFormat)
-
-		buf = bytes.NewBuffer(data[22:24])
-		binary.Read(buf, binary.LittleEndian, &wv.NumOfChan)
-
-		buf = bytes.NewBuffer(data[24:28])
-		binary.Read(buf, binary.LittleEndian, &wv.SampleRate)
-
-		buf = bytes.NewBuffer(data[28:32])
-		binary.Read(buf, binary.LittleEndian, &wv.BytesPerSec)
-
-		buf = bytes.NewBuffer(data[32:34])
-		binary.Read(buf, binary.LittleEndian, &wv.BlockAlign)
-
-		buf = bytes.NewBuffer(data[34:36])
-		binary.Read(buf, binary.LittleEndian, &wv.BitsPerSample)
-
-		wv.DataSize = uint32(wv.BitsPerSample/8) * uint32( wv.SampleRate)    
-
-		buf = bytes.NewBuffer(data[36:40])
-		binary.Read(buf, binary.LittleEndian, &wv.Subchunk2ID)
-
-		buf = bytes.NewBuffer(data[40:44])
-		binary.Read(buf, binary.LittleEndian, &wv.DataSize)
-
-		data2 := make([]byte, wv.DataSize)
-		_, err = f.Read(data2)
-		if err != nil {
-		           if err == io.EOF {
-		               panic ("file error")
-		           }
-		}
-		
-		wv.Data = data2;
-
-		return wv
+func (wv WaveFile) GetParams() (int, int, int, int) {
+	return wv.GetNChannels(), wv.GetSampWidth(), wv.GetFrameRate(), wv.GetNFrames()
 }
 
+func readWav(f *os.File) *WaveFile {
+	//make slice for the 44 byte header
+	data := make([]byte, 44)
+	_, err := f.Read(data)
+	if err != nil {
+		panic("file error")
+	}
+	wv := new(WaveFile)
+	wv.Riff = string(data[0:4])
+
+	// conversion needed to move the raw binary data to integer value
+	// create a buffer of bytes to hold the data
+	buf := bytes.NewBuffer(data[4:8])
+	// read the data in the buffer into the integer using LittleEndian conversion
+	binary.Read(buf, binary.LittleEndian, &wv.ChunkSize)
+	wv.Wave = string(data[8:12])
+	wv.Fmt = string(data[12:16])
+	buf = bytes.NewBuffer(data[16:20])
+	binary.Read(buf, binary.LittleEndian, &wv.Subchunk1Size)
+
+	buf = bytes.NewBuffer(data[20:22])
+	binary.Read(buf, binary.LittleEndian, &wv.AudioFormat)
+
+	buf = bytes.NewBuffer(data[22:24])
+	binary.Read(buf, binary.LittleEndian, &wv.NumOfChan)
+
+	buf = bytes.NewBuffer(data[24:28])
+	binary.Read(buf, binary.LittleEndian, &wv.SampleRate)
+
+	buf = bytes.NewBuffer(data[28:32])
+	binary.Read(buf, binary.LittleEndian, &wv.BytesPerSec)
+
+	buf = bytes.NewBuffer(data[32:34])
+	binary.Read(buf, binary.LittleEndian, &wv.BlockAlign)
+
+	buf = bytes.NewBuffer(data[34:36])
+	binary.Read(buf, binary.LittleEndian, &wv.BitsPerSample)
+
+	wv.DataSize = uint32(wv.BitsPerSample/8) * uint32(wv.SampleRate)
+
+	buf = bytes.NewBuffer(data[36:40])
+	binary.Read(buf, binary.LittleEndian, &wv.Subchunk2ID)
+
+	buf = bytes.NewBuffer(data[40:44])
+	binary.Read(buf, binary.LittleEndian, &wv.DataSize)
+
+	data2 := make([]byte, wv.DataSize)
+	_, err = f.Read(data2)
+	if err != nil {
+		if err == io.EOF {
+			panic("file error")
+		}
+	}
+
+	wv.Data = data2
+
+	return wv
+}
